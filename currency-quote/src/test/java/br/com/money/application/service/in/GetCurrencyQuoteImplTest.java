2f4c8b707310f57a4c8773bc64701079e15646cd
Wesley Pereira <jimmmisss@gmail.com>
2021-09-11 18:21:53 -0300

    Acertando teste simples de integração

--- currency-quote/src/test/java/br/com/money/application/service/in/GetCurrencyQuoteImplTest.java
+++ currency-quote/src/test/java/br/com/money/application/service/in/GetCurrencyQuoteImplTest.java
@@ -1,9 +1,13 @@
 package br.com.money.application.service.in;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -11,10 +15,18 @@ class GetCurrencyQuoteImplTest {
 
     @Test
     public void givenCurrenciesThenReturnJson() {
-        given()
-                .when().get("/json/last/currencies/")
+        String currency = "USD-BRL";
+        String money = "USD";
+        BigDecimal valueProduct = BigDecimal.valueOf(90);
+        String valueProductInCurrency = "474.264";
+        String prettify = given()
+                .when()
+                .get("/v1/currency/{currency}/value/{valueProduct}", currency, valueProduct)
                 .then()
                 .statusCode(200)
-                .body("size()", is(1), "[0].code", is("USDBRL"));
+                .body("$.size()", is(1),
+                        "[0].code", is(money))
+                .extract().response().jsonPath().prettify();
+        System.out.println(prettify);
     }
 }
