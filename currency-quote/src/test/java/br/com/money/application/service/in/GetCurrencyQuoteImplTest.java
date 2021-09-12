7141db36d4ab463a966668f40d5717d3fd641138
Wesley Pereira <jimmmisss@gmail.com>
2021-09-11 21:54:28 -0300

    Melhorando teste

--- currency-quote/src/test/java/br/com/money/application/service/in/GetCurrencyQuoteImplTest.java
+++ currency-quote/src/test/java/br/com/money/application/service/in/GetCurrencyQuoteImplTest.java
@@ -1,14 +1,13 @@
 package br.com.money.application.service.in;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 class GetCurrencyQuoteImplTest {
@@ -18,14 +17,14 @@ class GetCurrencyQuoteImplTest {
         String currency = "USD-BRL";
         String money = "USD";
         BigDecimal valueProduct = BigDecimal.valueOf(90);
-        String valueProductInCurrency = "474.264";
+        Double valueProductInCurrency = 474.264;
         String prettify = given()
                 .when()
                 .get("/v1/currency/{currency}/value/{valueProduct}", currency, valueProduct)
                 .then()
-                .statusCode(200)
-                .body("$.size()", is(1),
-                        "[0].code", is(money))
+                .body("size()", equalTo(1))
+                .body("code", hasItem(money))
+                .statusCode(Response.Status.OK.getStatusCode())
                 .extract().response().jsonPath().prettify();
         System.out.println(prettify);
     }
